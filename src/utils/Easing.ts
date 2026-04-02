6d03f02b00a7010a26ad53c6ea99ea3151d74827
Vaclav Vancura <commit@vancura.dev>
2026-04-02 15:11:56 +0200

    fix(utils): add exhaustive never check to applyEasing switch
    Signed-off-by: Vaclav Vancura <commit@vancura.dev>

--- src/utils/Easing.ts
+++ src/utils/Easing.ts
@@ -31,6 +31,10 @@ export function applyEasing(t: number, easing: EasingFunction): number {
             return t * (2 - t);
         case 'ease-in-out':
             return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        default: {
+            const exhaustive: never = easing;
+            throw new Error(`Unsupported easing: ${exhaustive}`);
+        }
     }
 }
 
